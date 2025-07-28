f7010afa1f7040cb72c9bb871dae34ea9a1d9603
Dewayne VanHoozer <dvanhoozer@gmail.com>
2025-07-28 10:36:45 -0500

    Remove Ragdoll::Core files and configuration
    This commit prunes all files related to the Ragdoll::Core. This is part of
    the effort to refactor and streamline our application by removing unused
    or obsolete components.
    
    - Deleted `ragdoll.yml` configuration file.
    - Removed `ragdoll.rb` module file and all its methods.
    - Eliminated `api.rb` class file and its associated methods.

--- lib/ragdoll/rails/version.rb
+++ lib/ragdoll/rails/version.rb
@@ -4,6 +4,6 @@
 
 module Ragdoll
   module Rails
-    VERSION = "1.0.0"
+    VERSION = "0.0.1"
   end
 end
--- lib/ragdoll/version.rb
+++
@@ -1,6 +0,0 @@
-# ragdoll/version.rb
-# This file is shared between ragdoll-core, ragdoll-cli and ragdoll-rails
-
-module Ragdoll
-  VERSION = '0.0.1'
-end
